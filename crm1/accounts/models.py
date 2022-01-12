734ada032b62fdacc33894eb5bcb621676c9fee4
Nikhil Raj <nikhil.raj.aiml25@heritageit.edu.in>
2022-01-12 20:10:59 +0530

    Database relationship setup

--- crm1/accounts/models.py
+++ crm1/accounts/models.py
@@ -13,6 +13,13 @@ class Customer(models.Model):
     def __str__(self):
         return self.name
 
+class Tag(models.Model):
+    name = models.CharField(max_length=200, null=True)
+   
+
+    def __str__(self):
+        return self.name
+
 class Product(models.Model):
     CATEGORY = (
         ('Indoor', 'Indoor'),
@@ -22,8 +29,14 @@ class Product(models.Model):
     name = models.CharField(max_length=200, null=True)
     price = models.FloatField(null=True)
     category = models.CharField(max_length=200, null=True, choices=CATEGORY)
-    description = models.CharField(max_length=200, null=True)
+    description = models.CharField(max_length=200, null=True, blank=True)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
+    tags = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return self.name
+
+
 
 
 class Order(models.Model):
@@ -33,5 +46,13 @@ class Order(models.Model):
         ('Delivered','Delivered')
     )
 
+    customer = models.ForeignKey(Customer, null=True,on_delete= models.SET_NULL)
+
+    product = models.ForeignKey(Product, null=True,on_delete= models.SET_NULL)
+
     date_created = models.DateTimeField(auto_now_add=True, null=True)
-    status = models.CharField(max_length=200, null=True)
+    status = models.CharField(max_length=200, null=True, choices=STATUS)
+
+    
+
+    
