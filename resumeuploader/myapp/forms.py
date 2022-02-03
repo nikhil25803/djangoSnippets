6bed0c779e00528e034122e5a16c3131968b3d48
Nikhil Raj <nikhil.raj.aiml25@heritageit.edu.in>
2022-02-03 13:03:15 +0530

    Added Labels and Widgets

--- resumeuploader/myapp/forms.py
+++ resumeuploader/myapp/forms.py
@@ -1,9 +1,34 @@
 from attr import fields
 from django import forms
+from matplotlib import widgets
 from .models import Resume
 
 
 class ResumeForm(forms.ModelForm):
     class Meta:
         model = Resume
-        fields = ['name', 'dob', 'gender', 'locality', 'city', 'pin', 'mobile', 'job_city', 'profile_image', 'my_file']
+        fields = ['name', 'dob', 'gender', 'locality', 'city', 'pin',
+                  'state', 'mobile', 'job_city', 'profile_image', 'my_file']
+
+        labels = {'name': 'Full Name', 'dob': 'Date of Birth', 'pin': 'Pin Code', 'mobile': 'Mobile No',
+                  'email': 'Email ID', 'profile_iamge': 'Profile Image', 'my_file': 'Documents'}
+
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            
+            'dob': forms.DateInput(attrs={'class': 'form-control', 'id': 'datepicker'}),
+            
+            'locality': forms.TextInput(attrs={'class': 'form-control'}),
+            
+            'city': forms.TextInput(attrs={'class': 'form-control'}),
+            
+            'pin': forms.NumberInput(attrs={'class': 'form-control'}),
+            
+            'state': forms.Select(attrs={'class': 'form-select'}),
+            
+            'mobile': forms.NumberInput(attrs={'class': 'form-control'}),
+            
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+            
+
+        }
--- resumeuploader/myapp/views.py
+++ resumeuploader/myapp/views.py
@@ -1,3 +1,14 @@
+import imp
 from django.shortcuts import render
 
 # Create your views here.
+
+from .forms import ResumeForm
+from .models import Resume
+from django.views import View
+
+
+class HomeView(View):
+    def get(self, request):
+        form  = ResumeForm()
+        return render(request, "myapp/home.html", {'form':form})
