fa0daa3181fc845abec8591b4d847f57ae843f00
Nikhil Raj <nikhil.raj.aiml25@heritageit.edu.in>
2022-02-04 18:54:36 +0530

    Design using Bootstrap

--- project/prapp/forms.py
+++ project/prapp/forms.py
@@ -1,9 +1,30 @@
 from distutils.command.build_scripts import first_line_re
 from attr import field
 from django import forms
+from matplotlib import widgets
 from .models import Project
 
 class ProjectForm(forms.ModelForm):
     class Meta:
         model = Project
-        fields = ['name','college','year','prname','prdes','stack','lang','primg', 'repo', 'live', 'docs']
+        fields = ['name','college','prname','prdes','stack','lang','primg', 'repo', 'live', 'docs']
+
+        labels = {'name':'Name of the Studets','college':'College Name', 'year':'Year', 'prname':'Project Name', 'prdes':'Project Description', 'stack':'Tech Stack Used', 'lang':'Language Used', 'primg':'Project Image', 'repo':'GitHub Repo Link','live':'Live Link', 'docs':'Documentation'}
+
+        widgets={
+            'name':forms.TextInput(attrs={'class':'form-control'}),
+
+            'college':forms.TextInput(attrs={'class':'form-control'}),
+
+            'prname':forms.TextInput(attrs={'class':'form-control'}),
+
+            'prdes':forms.TextInput(attrs={'class':'form-control'}),
+
+            'repo':forms.TextInput(attrs={'class':'form-control'}),
+
+            'live':forms.TextInput(attrs={'class':'form-control'}),
+
+            'docs':forms.TextInput(attrs={'class':'form-control'}),
+
+
+        }
