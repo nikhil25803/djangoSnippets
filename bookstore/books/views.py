881c15d3e9aebc30f7b6c8922a401a159b0c0133
Nikhil Raj <nikhil.raj.aiml25@heritageit.edu.in>
2022-02-28 11:24:36 +0530

    JSoN File Setup

--- bookstore/books/views.py
+++ bookstore/books/views.py
@@ -1,7 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+import json
+
+
+booksData = open('J:/Programming/Git & GitHub/Django_File0/Django_Docs/bookstore/books.json').read()
+
+data = json.loads(booksData)
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse('Hello Books App')
+    context = {'books':data}
+    return render(request, 'books/html/index.html', context)
