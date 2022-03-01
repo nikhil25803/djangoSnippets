074ee2f8531198315bd8e8ce878304bc0287c0ee
Nikhil Raj <nikhil.raj.aiml25@heritageit.edu.in>
2022-03-01 19:39:42 +0530

    First Migration

--- bookstore/books/models.py
+++ bookstore/books/models.py
@@ -1,3 +1,10 @@
+from pyexpat import model
+from statistics import mode
 from django.db import models
+from matplotlib.pyplot import title
 
 # Create your models here.
+
+class Book(models.Model):
+
+    title = models.CharField(max_length=100)
