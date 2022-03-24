d1cc4d6136c3007e3d3f899ab42907b48f2de14a
Nikhil Raj <nikhil.raj.aiml25@heritageit.edu.in>
2022-03-25 02:31:21 +0530

    Template Setup

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,13 +1,41 @@
-import imp
+# import imp
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+posts = [
+    {
+        'author': 'Nikhil Raj',
+        'title': 'Blog Post 1',
+        'content': 'My first blog post',
+        'date_posted': 'August 20, 2020',
+    },
+
+    {
+        'author': 'Nikhil Raj 02',
+        'title': 'Blog Post 02',
+        'content': 'My Second blog post',
+        'date_posted': 'August 20, 2021',
+    },
+    {
+        'author': 'Nikhil Raj 03',
+        'title': 'Blog Post 03',
+        'content': 'My Second blog post',
+        'date_posted': 'August 20, 2021',
+    },
+
+
+]
+
 def home(request):
 
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts,
+    }
+
+    return render(request, 'blog/index.html', context)
 
 def about(request):
 
-    return HttpResponse('<h1>Blog About Page</h2>')
+    return render(request, 'blog/about.html', {'title':'About'})
 
