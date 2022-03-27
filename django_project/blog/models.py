f5c67641a4115a419408035782734c13eded4933
Nikhil Raj <nikhil.raj.aiml25@heritageit.edu.in>
2022-03-27 14:11:47 +0530

    Template Setup

--- django_project/blog/models.py
+++ django_project/blog/models.py
@@ -1,3 +1,19 @@
+# from email import contentmanager
+# from pyexpat import model
 from django.db import models
+# from matplotlib.pyplot import title
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+# Post model
+
+class Post(models.Model):
+
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    date_posted = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,36 +1,14 @@
 # import imp
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 # Create your views here.
 
-posts = [
-    {
-        'author': 'Nikhil Raj',
-        'title': 'Blog Post 1',
-        'content': 'My first blog post',
-        'date_posted': 'August 20, 2020',
-    },
-
-    {
-        'author': 'Nikhil Raj 02',
-        'title': 'Blog Post 02',
-        'content': 'My Second blog post',
-        'date_posted': 'August 20, 2021',
-    },
-    {
-        'author': 'Nikhil Raj 03',
-        'title': 'Blog Post 03',
-        'content': 'My Second blog post',
-        'date_posted': 'August 20, 2021',
-    },
-
-
-]
 
 def home(request):
 
     context = {
-        'posts': posts,
+        'posts': Post.objects.all()
     }
 
     return render(request, 'blog/index.html', context)
