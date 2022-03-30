3da570e7a005bb074ae26770be24134a564a0a36
Nikhil Raj <nikhil.raj.aiml25@heritageit.edu.in>
2022-03-31 01:01:45 +0530

    Login/Logout Added

--- django_project/users/views.py
+++ django_project/users/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.forms import UserCreationForm
 # from telegram import User
 from django.contrib import messages
 from .forms import UserRegisterForm
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
@@ -15,8 +16,8 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account Created For {username}')
-            return redirect('blog-home')
+            messages.success(request, f'Your account have been created')
+            return redirect('login')
 
 
     else:
@@ -27,3 +28,8 @@ def register(request):
 
 
 # messages.debug
+
+@login_required
+def profile(request):
+
+    return render(request, 'users/profile.html')
