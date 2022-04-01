f02ac861f909e31d8fec6da3d65d83e2fe93bcb5
Nikhil Raj <nikhil.raj.aiml25@heritageit.edu.in>
2022-04-01 23:29:56 +0530

    Profile Pic Upload

--- django_project/users/models.py
+++ django_project/users/models.py
@@ -1,3 +1,14 @@
+from distutils.command.upload import upload
+from email.policy import default
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(default='default.jps', upload_to='profile_pics')
+
+    def __str__(self):
+        return f"{self.user.username} Profile"
+
